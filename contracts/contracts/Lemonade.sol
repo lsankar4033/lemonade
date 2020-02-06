97b8aa0c0db8436b6fc0f7049becf14064c1e640
lsankar4033 <lsankar4033@gmail.com>
2020-02-05 16:33:03 -0800

    Remove unused code that was helpful as a reference

--- contracts/contracts/Lemonade.sol
+++ contracts/contracts/Lemonade.sol
@@ -21,28 +21,8 @@ contract Lemonade {
         msg.sender.transfer(val);
     }
 
-    // TODO: Remove. This is just for testing signing + recovery. I.e. determining whether I need a normal Eth
-    // prefix. Same for 'recover'
-    function recover_nv(uint128 nonce, uint128 val, uint8 v, bytes32 r, bytes32 s) external pure returns (address) {
-        bytes32 hash = create_prefixed_hash(nonce, val);
-        address signer = ecrecover(hash, v, r, s);
-
-        return signer;
-    }
-
-
     // method that creates the bytes object that we expect the signature to be of
     function create_prefixed_hash(uint128 nonce, uint128 val) public pure returns (bytes32) {
-        // NOTE: packing nonce and val into a nice 32byte array
-        //bytes memory data = abi.encodePacked(nonce, val);
-        //require(data.length == 32, "packed data should be 32 bytes");
-
-        //bytes32 message;
-
-        //assembly {
-            //message := mload(add(data, 0x20))
-        //}
-
         bytes memory hashPrefix = "\x19Ethereum Signed Message:\n32";
         bytes memory message = abi.encodePacked(hashPrefix, nonce, val);
 
