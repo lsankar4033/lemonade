1bcdadefb0c0faf30dbb5acef4a55cdeb80fa130
lsankar4033 <lsankar4033@gmail.com>
2020-02-05 16:32:33 -0800

    Code for creating and verifying signatures that's consistent b/w python and solidity

--- contracts/contracts/Lemonade.sol
+++ contracts/contracts/Lemonade.sol
@@ -13,12 +13,41 @@ contract Lemonade {
         require(nonce == nextNonce, "Nonce must be the expected next nonce");
         require(address(this).balance > val, "Contract must have enough balance to cover val");
 
-        bytes memory data = abi.encodePacked(nonce, val);
-        bytes32 hashed = keccak256(data);
-
-        address signer = ecrecover(hashed, v, r, s);
+        bytes32 hash = create_prefixed_hash(nonce, val);
+        address signer = ecrecover(hash, v, r, s);
         require(signer == owner, "Data must be signed by contract owner");
 
+        nextNonce += 1;
         msg.sender.transfer(val);
+    }
+
+    // TODO: Remove. This is just for testing signing + recovery. I.e. determining whether I need a normal Eth
+    // prefix. Same for 'recover'
+    function recover_nv(uint128 nonce, uint128 val, uint8 v, bytes32 r, bytes32 s) external pure returns (address) {
+        bytes32 hash = create_prefixed_hash(nonce, val);
+        address signer = ecrecover(hash, v, r, s);
+
+        return signer;
+    }
+
+
+    // method that creates the bytes object that we expect the signature to be of
+    function create_prefixed_hash(uint128 nonce, uint128 val) public pure returns (bytes32) {
+        // NOTE: packing nonce and val into a nice 32byte array
+        //bytes memory data = abi.encodePacked(nonce, val);
+        //require(data.length == 32, "packed data should be 32 bytes");
+
+        //bytes32 message;
+
+        //assembly {
+            //message := mload(add(data, 0x20))
+        //}
+
+        bytes memory hashPrefix = "\x19Ethereum Signed Message:\n32";
+        bytes memory message = abi.encodePacked(hashPrefix, nonce, val);
+
+        bytes32 prefixedHash = keccak256(message);
+
+        return prefixedHash;
     }
 }
---
+++ python/lemonade/lemonade.py
@@ -0,0 +1,30 @@
+from eth_abi.packed import encode_abi_packed
+from eth_account.messages import encode_defunct
+from hashlib import sha3_256
+
+from web3 import Web3
+from web3.auto import w3
+
+ganache_privkey = bytes.fromhex('f85ea1dfdee17cde71ec01ca3f333d40b58fa6501df928393416115924d75daf')
+
+
+def to_32byte_hex(val):
+    return Web3.toHex(Web3.toBytes(val).rjust(32, b'\0'))
+
+
+def pack_data(nonce, val):
+    return encode_abi_packed(['uint128', 'uint128'], (nonce, val))
+
+
+# NOTE: Doesn't do 'Ethereum signed message: ' prefixing yet!
+def sign_data(packed_data, privkey=ganache_privkey):
+    signable_message = encode_defunct(primitive=packed_data)
+
+    # TODO: not the right format!
+    signed = w3.eth.account.sign_message(signable_message, private_key=privkey)
+    return (
+        Web3.toHex(signed.messageHash),
+        signed.v,
+        to_32byte_hex(signed.r),
+        to_32byte_hex(signed.s)
+    )
